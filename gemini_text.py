2570ebcd115800540a9b1b6d1db4a54e98527ebd
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-02-17 22:24:43 -0500

    Update gemini_text.py
    - [v1.1.0]: Codebase refactor and CLI support.

--- gemini_text.py
+++ gemini_text.py
@@ -1,40 +1,27 @@
-import os
-import requests
-from dotenv import load_dotenv
+from loading import Loading
+from base_api import BaseAPI
 
-load_dotenv()
-
-class TextAPI:
-    def __init__(self):
-        self.api_key = os.getenv('API_KEY')
-        if not self.api_key:
-            raise ValueError("API key not found. Set the API_KEY environment variable.")
-
-    def response(self, text_prompt: str) -> str:
-        try:
-            return self._response(text_prompt)
-        except Exception as e:
-            raise ValueError(f"Error generating caption: {e}")
-
-    def _response(self, text_prompt: str) -> str:
-        url = f"https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key={self.api_key}"
-        headers = { "Content-Type": "application/json" }
+class TextAPI(BaseAPI):
+    def text(self, text_prompt):
+        url = "https://generativelanguage.googleapis.com/v1/models/gemini-1.0-pro-latest:generateContent"
         payload = {
             "contents": [
                 {
                     "parts": [
-                        { "text": text_prompt }
+                        {"text": text_prompt}
                     ]
                 }
             ]
         }
-        response = self._request(url, headers, payload)
-        print(response["candidates"][0]["content"]["parts"][0]["text"])
-    
-    def _request(self, url: str, headers: dict, payload: dict) -> dict:
+        loading = Loading()
+        loading.start()
+
         try:
-            response = requests.post(url, headers=headers, json=payload)
-            response.raise_for_status()
-            return response.json()
-        except requests.RequestException as e:
-            raise ValueError(f"Error communicating with API: {e}")
+            response = self.post(f"{url}?key={self.api_key}", payload)
+        finally:
+            loading.stop()
+
+        if response and "candidates" in response and response["candidates"]:
+            print(response["candidates"][0]["content"]["parts"][0]["text"])
+        else:
+            print("No response or error occurred.")
