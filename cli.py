ea17826fef49d42fe6a313f83a9f4f539f62c95e
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-03-08 15:42:08 -0500

    Create cli.py
    - Renamed and updated `cli.py` for a comprehensive command-line interface supporting chat, text, and vision modes with extensive customization options.

---
+++ cli.py
@@ -0,0 +1,70 @@
+import argparse
+from gemini import Chat, Text, Vision
+
+def main():
+    class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+                      argparse.RawDescriptionHelpFormatter):
+        pass
+    parser = argparse.ArgumentParser(
+        description="""
+    ------------------------------------------------------------------
+                            Gemini AI Toolkit                          
+                   API Wrapper & Command-line Interface               
+                          [v1.2.0] by @rmncldyo                      
+    ------------------------------------------------------------------
+
+    Gemini AI toolit is an API wrapper and command-line interface for Google's latest Gemini Pro and Gemini Ultra large-language models.
+
+    | Option(s)                | Description                               | Example Usage                                                                 |
+    |--------------------------|-------------------------------------------|-------------------------------------------------------------------------------|
+    | -c,  --chat              | Enable chat mode                          | --chat                                                                        |
+    | -t,  --text              | Enable text mode                          | --text                                                                        |
+    | -v,  --vision            | Enable vision mode                        | --vision                                                                      |
+    | -p,  --prompt            | User prompt                               | --prompt "Write a story about a magic backpack."                              |
+    | -i,  --image             | Image file path or url                    | --image "path_or_url_goes_here"                                               |
+    | -a,  --api_key           | Gemini API key for authentication         | --api_key "api_key_goes_here"                                                 |
+    | -m,  --model             | The model you would like to use           | --model "model_name_goes_here"                                                |
+    | -s,  --stream            | Enable streaming mode for responses       | --stream                                                                      |
+    | -mt, --max_tokens        | Maximum number of tokens to generate      | --max_tokens 1024                                                             |
+    | -tm, --temperature       | Sampling temperature                      | --temperature 0.7                                                             |
+    | -tp, --top_p             | Nucleus sampling threshold                | --top_p 0.9                                                                   |
+    | -tk, --top_k             | Top-k sampling threshold                  | --top_k 40                                                                    |
+    | -cc, --candidate_count   | Number of candidates to generate          | --candidate_count 1                                                           |
+    | -ss, --stop_sequences    | Stop sequences for completion             | --stop_sequences ["SAFETY_WORD_1", "SAFETY_WORD_2"]                           |
+    | -sc, --safety_categories | Safety categories for filtering           | --safety_categories ["HARM_CATEGORY_HARASSMENT", "HARM_CATEGORY_HATE_SPEECH"] |
+    | -st, --safety_thresholds | Safety thresholds for filtering           | --safety_thresholds ["BLOCK_ONLY_HIGH", "BLOCK_NONE"]                         |
+    """,
+        formatter_class=CustomFormatter,
+        epilog="For detailed usage information, visit our ReadMe here: github.com/RMNCLDYO/gemini-ai-toolkit"
+    )
+    parser.add_argument('-c', '--chat', action='store_true', help='Enable chat mode')
+    parser.add_argument('-t', '--text', action='store_true', help='Enable text mode')
+    parser.add_argument('-v', '--vision', action='store_true', help='Enable vision mode')
+    parser.add_argument('-p', '--prompt', type=str, help='Text or Vision prompt', metavar='')
+    parser.add_argument('-i', '--image', type=str, help='Image file path or url', metavar='')
+    parser.add_argument('-a', '--api_key', type=str, help='Gemini API key for authentication', metavar='')
+    parser.add_argument('-m', '--model', type=str, default='gemini-1.0-pro-latest', help='The model you would like to use', metavar='')
+    parser.add_argument('-s', '--stream', action='store_true', help='Enable streaming mode for responses')
+    parser.add_argument('-mt', '--max_tokens', type=int, help='Maximum number of tokens to generate', metavar='')
+    parser.add_argument('-tm', '--temperature', type=float, help='Sampling temperature', metavar='')
+    parser.add_argument('-tp', '--top_p', type=float, help='Nucleus sampling threshold', metavar='')
+    parser.add_argument('-tk', '--top_k', type=int, help='Top-k sampling threshold', metavar='')
+    parser.add_argument('-cc', '--candidate_count', type=int, help='Number of candidates to generate', metavar='')
+    parser.add_argument('-ss', '--stop_sequences', type=str, nargs='+', help='Stop sequences for completion', metavar='')
+    parser.add_argument('-sc', '--saftey_categories', type=str, help='Safety category/categories for completion', metavar='')
+    parser.add_argument('-st', '--saftey_thresholds', type=str, help='Safety threshold(s) for completion', metavar='')
+
+    args = parser.parse_args()
+    
+    if args.chat:
+        Chat().run(args.api_key, args.model, args.prompt, args.max_tokens, args.temperature, args.top_p, args.top_k, args.candidate_count, args.stop_sequences, args.saftey_categories, args.saftey_thresholds, args.stream)
+    elif args.text:
+        Text().run(args.api_key, args.model, args.prompt, args.max_tokens, args.temperature, args.top_p, args.top_k, args.candidate_count, args.stop_sequences, args.saftey_categories, args.saftey_thresholds, args.stream)
+    elif args.vision:
+        Vision().run(args.api_key, args.model, args.prompt, args.image, args.max_tokens, args.temperature, args.top_p, args.top_k, args.candidate_count, args.stop_sequences, args.saftey_categories, args.saftey_thresholds, args.stream)
+    else:
+        print("Error: Please specify a mode to use. Use --help for more information.")
+        exit()
+
+if __name__ == "__main__":
+    main()
