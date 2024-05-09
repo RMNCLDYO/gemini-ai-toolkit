b9703e234c94a5ecd01400076c8f46635bf58157
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-05-08 20:36:25 -0400

    Update example_audio.py

--- examples/example_audio.py
+++ examples/example_audio.py
@@ -1,3 +1,3 @@
 from gemini import Audio
 
-Audio().run(prompt="Describe this audio", media="https://cdn.pixabay.com/audio/2024/02/26/audio_423dd4facf.mp3")
+Audio().run(prompt="Describe this audio.", media="https://cdn.pixabay.com/audio/2024/02/26/audio_423dd4facf.mp3")
