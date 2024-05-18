3febcf10f9ee526306e36d0a99ac623c207b120a
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-05-17 21:24:31 -0400

    Update example_vision.py

--- examples/example_vision.py
+++ examples/example_vision.py
@@ -1,3 +1,3 @@
 from gemini import Vision
 
-Vision().run(prompt="Describe this image.", media="https://upload.wikimedia.org/wikipedia/commons/a/a7/Camponotus_flavomarginatus_ant.jpg")
+Vision().run(prompt="Describe the image with a creative description.", media="https://storage.googleapis.com/generativeai-downloads/images/jetpack.jpg")
