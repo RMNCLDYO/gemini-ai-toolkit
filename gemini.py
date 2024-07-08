7404420a051c54b08afa5a3d094e5d92754e4a5e
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-07-07 20:33:04 -0400

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -198,7 +198,7 @@ class Text:
             "topP": float(top_p) if top_p else None,
             "topK": int(top_k) if top_k else None,
             "maxOutputTokens": int(max_tokens) if max_tokens else None,
-            "candidateCount": int(candidate_count) if candidate_count else None, # Candidate count is not supported due to a bug in the API
+            "candidateCount": int(candidate_count) if candidate_count else None, # Candidate count is not supported yet supported by the API
             "stopSequences": stop_sequences if stop_sequences else None,
             "response_mime_type": json_mode if json_mode else None # JSON mode is only supported in Gemini 1.5 Pro and later
         }
