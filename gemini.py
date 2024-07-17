ca7c0b254b5f3dba508b3caa5c581b958fed05f2
RMNCLDYO <153027766+RMNCLDYO@users.noreply.github.com>
2024-07-16 20:09:45 -0400

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -357,7 +357,7 @@ class Vision:
 
         # Candidate count is not yet supported by the API
         if candidate_count and candidate_count > 1:
-            print("Error: Candidate count is not supported due to a bug in the API. Please use a candidate count of 1, or remove it completely.")
+            print("Error: Candidate count is not yet supported by the API. Please use a candidate count of 1, or remove it completely.")
             exit(1)
 
         conversation_data = {}
