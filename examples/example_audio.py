d97547a878659f01c38f44e557cdf05ddd5599b1
RMNCLDYO <rmncldyo@gmail.com>
2024-07-31 12:09:54 -0400

    Delete example_audio.py
    - Removed outdated example file.

--- examples/example_audio.py
+++
@@ -1,3 +0,0 @@
-from gemini import Audio
-
-Audio().run(prompt="Listen carefully to the following audio file. Provide a brief summary.", media="https://storage.googleapis.com/generativeai-downloads/data/State_of_the_Union_Address_30_January_1961.mp3")
